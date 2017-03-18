3f21158d1bef5a0414e3af36712ad6d83f34da86
ElGrisX <reyescarmona@gmail.com>
2017-03-18 13:47:29 -0600

    Creacion del script de monitoreo

---
+++ src/monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Am,bos Comandos son equivalentes
+current_pwd=`pwd`
+current_pwd=$(pwd)
+cd /usr/share/doc
+echo Reporte: >>/tmp/reporte
+date >> /tmp/reporte
+pwd >> /tmp/reporte
+du -sh  >> /tmp/reporte
+
+echo Path de ejecucion  `pwd`
+
+cd $current_pwd
